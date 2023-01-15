0f35f6e4f572e947a7b6f4d321e82c2b7f3ac16c
kiprotich <kiprotichmichael570@gmail.com>
2023-01-15 17:12:36 +0300

    setting up spring boot project

---
+++ src/main/java/aboiyon/springboot/tutorialtracker/TutorialTrackerApplication.java
@@ -0,0 +1,13 @@
+package aboiyon.springboot.tutorialtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aboiyon/springboot/tutorialtracker/TutorialTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package aboiyon.springboot.tutorialtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
