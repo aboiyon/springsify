06fa36b568cbf088cf06125e295c96887e2ccee0
kiprotich <kiprotichmichael570@gmail.com>
2023-01-15 17:38:39 +0300

    creating a repository to interact with Tutorials from the database

--- src/main/java/repository/TutorialRepository.java
+++ src/main/java/repository/TutorialRepository.java
@@ -1,4 +1,8 @@
 package repository;
 
-public interface TutorialRepository {
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long>{
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
 }
